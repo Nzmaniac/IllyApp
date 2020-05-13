a70619e4c74dd25f99f1b47a76757a30661b12b7
Dave Chapman <infiniteloopnz@gmail.com>
2020-05-13 19:24:11 +1200

    initial Commit

---
+++ IllyApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using IllyApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IllyApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Value> Values {get ; set; }
+    }
+}
